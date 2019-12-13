3e1c3706e82d953a08886ef252fdcfabf3f9d5b5
MTianX <ad769672898@163.com>
2019-12-13 11:15:51 +0800

    添加页面，实现wifi扫描功能

--- app/src/main/java/com/example/simplebackup/MainActivity.java
+++ app/src/main/java/com/example/simplebackup/MainActivity.java
@@ -6,20 +6,27 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.simplebackup.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    MyViewModel myViewModel;
+   // public MyViewModel myViewModel;
     ActivityMainBinding binding;
     private ListView listView;
     private BaseAdapter listItemAdapter;
     String TAG = "TAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        NavController controller = Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this,controller);
+       // binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 /*        ActivityMainBinding mbinding = ActivityMainBinding.inflate(getLayoutInflater());
         myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);*/
 /*        binding.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 
-
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController controller = Navigation.findNavController(this,R.id.fragment);
+        return  controller.navigateUp();
+        //return super.onSupportNavigateUp();
+    }
 }
--- app/src/main/java/com/example/simplebackup/WifiFragment.java
+++ app/src/main/java/com/example/simplebackup/WifiFragment.java
@@ -1,19 +1,33 @@
 package com.example.simplebackup;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WifiFragment extends Fragment {
-
+    private ListView listView;
+    private BaseAdapter listItemAdapter;
+    MyViewModel myViewModel;
+    String TAG = "TAG";
 
     public WifiFragment() {
         // Required empty public constructor
@@ -27,4 +41,22 @@ public class WifiFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_wifi, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final ListView listview1 = Objects.requireNonNull(getView()).findViewById(R.id.listview);
+        Button button_check = Objects.requireNonNull(getView()).findViewById(R.id.button4);
+        myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
+        ArrayList<MyViewModel.Item>    list = myViewModel.getSsidItem();
+        listItemAdapter = new MyBaseAdapter(getActivity(),list);
+        listview1.setAdapter(listItemAdapter);
+        button_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
 }
+
